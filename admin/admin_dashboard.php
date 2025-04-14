bed406cce47247b2500ff4a68122a4abf4090535
‪DaryaN DaNa‬‏ <134648008+DarYaN2@users.noreply.github.com>
2025-04-14 12:44:14 -0800

    Update admin_dashboard.php

--- admin/admin_dashboard.php
+++ admin/admin_dashboard.php
@@ -8,6 +8,26 @@ if (!isset($_SESSION['user_id']) || !isset($_SESSION['is_admin']) || $_SESSION['
 
 include('/var/www/coffee.siciit.com/config.php');
 ?>
+<?php
+$lowStockStmt = $pdo->query("SELECT product_name, product_quantity FROM storage WHERE product_quantity <= 5");
+$lowStockItems = $lowStockStmt->fetchAll();
+?>
+
+<?php if ($lowStockItems): ?>
+  <div style="
+    padding: 
+    5px;
+	font-size:18px;
+    color: #fd3f14;
+    text-shadow:0px 0px 12px red;
+    margin-bottom: 30px;
+    ">
+        <strong> ئاگاداری کەمبوونی کاڵا</strong>
+        <ul>
+                            <li>hot_chocolate — تەنها 2 دانە ماوە!</li>
+                    </ul>
+    </div>
+<?php endif; ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -16,6 +36,7 @@ include('/var/www/coffee.siciit.com/config.php');
     <meta charset="UTF-8">
     <title>داشبۆردی ئەدمین</title>
     <link rel="stylesheet" href="style.css">
+	<style>body{display:block;}</style>
 </head>
 <body>
     <div class="dashboard">
@@ -30,7 +51,7 @@ include('/var/www/coffee.siciit.com/config.php');
 <li><a href="add_new.php">زیاد کردنی بەرهەمی نوێ</a></li><br>
             <li><a href="view_sales.php">بینینی فرۆشتنەکان</a></li><br>
             <li><a href="sales_chart.php">خشتەی فرۆشتنەکان</a></li><br>
-            <li><a href="admin_logout.php">چوونەدەرەوە</a></li>
+            <li><a href="admin_logout.php"><span style="color: #ff7200;text-shadow: 0px 0px 10px #ff9b4b;">چوونەدەرەوە</a></li>
         </ul>
     </div>
 </body>
